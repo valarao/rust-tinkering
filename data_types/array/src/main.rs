da709b8bdc5903bd56d6d9be7e6b321aa6bb5dce
Miguel Valarao <miguel.valarao@gmail.com>
2020-07-12 01:42:23 -0700

    Data types

---
+++ data_types/array/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+    
+    // Type; # of Elements 
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // [3, 3, 3, 3, 3]
+    let a = [3; 5];
+
+    let first = a[0];
+    let second = a[1];
+}
---
+++ data_types/tuple/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let tup = (500, 6.4, 1);
+
+    // Destructuring
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let five_hundred = tup.0;
+
+    let six_point_four = tup.1;
+
+    let one = tup.2;
+}
