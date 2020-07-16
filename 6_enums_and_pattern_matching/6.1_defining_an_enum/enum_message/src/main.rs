05ecf992a6901b353925ae1672acfae29a0ed5bc
Miguel Valarao <miguel.valarao@gmail.com>
2020-07-15 19:56:11 -0700

    match control flow operator

--- 6_enums_and_pattern_matching/6.1_defining_an_enum/enum_message/src/main.rs
+++ 6_enums_and_pattern_matching/6.1_defining_an_enum/enum_message/src/main.rs
@@ -1,3 +1,17 @@
+enum Message {
+    Quit,
+    Move {x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body defined here
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
--- 6_enums_and_pattern_matching/6.1_defining_an_enum/enum_option/src/main.rs
+++ 6_enums_and_pattern_matching/6.1_defining_an_enum/enum_option/src/main.rs
@@ -1,11 +1,12 @@
-enum Option<T> {
-    Some(T),
-    None,
-}
-
 fn main() {
     let some_number = Some(5);
     let some_string = Some("a string");
 
     let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    let sum = x + y.unwrap();
+    println!("{}", sum);
 }
