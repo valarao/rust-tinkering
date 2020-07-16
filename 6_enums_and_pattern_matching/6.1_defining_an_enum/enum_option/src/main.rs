1eaf5b6c64e5ea084870ba87c258a784e6780045
Miguel Valarao <miguel.valarao@gmail.com>
2020-07-15 19:38:48 -0700

    Defining an enum

---
+++ 6_enums_and_pattern_matching/6.1_defining_an_enum/enum_option/src/main.rs
@@ -0,0 +1,11 @@
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+fn main() {
+    let some_number = Some(5);
+    let some_string = Some("a string");
+
+    let absent_number: Option<i32> = None;
+}
