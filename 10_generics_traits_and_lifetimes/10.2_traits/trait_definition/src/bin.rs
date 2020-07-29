f24e74efd3a925d716ac92993c96814dba7549ee
Miguel Valarao <miguel.valarao@gmail.com>
2020-07-29 01:35:26 -0700

    10.2 Traits

---
+++ 10_generics_traits_and_lifetimes/10.2_traits/trait_definition/src/bin.rs
@@ -0,0 +1,32 @@
+use mylib::Summary;
+use mylib::Tweet;
+use mylib::NewsArticle;
+
+pub fn notify(item: &impl Summary) {
+  println!("Breaking news! {}", item.summarize());
+}
+
+pub fn main() {
+  let tweet = Tweet {
+    username: String::from("horse_ebooks"),
+    content: String::from(
+        "of course, as you probably already know, people",
+    ),
+    reply: false,
+    retweet: false,
+  };
+
+  println!("1 new tweet: {}", tweet.summarize()); 
+  
+  let article = NewsArticle {
+    headline: String::from("Penguins win the Stanley Cup Championship!"),
+    location: String::from("Pittsburgh, PA, USA"),
+    author: String::from("Iceburgh"),
+    content: String::from(
+        "The Pittsburgh Penguins once again are the best \
+         hockey team in the NHL.",
+    ),
+  };
+
+  println!("New article available! {}", article.summarize());
+}
---
+++ 10_generics_traits_and_lifetimes/10.2_traits/trait_definition/src/lib.rs
@@ -0,0 +1,41 @@
+pub fn test() {
+    println!("Test");
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
