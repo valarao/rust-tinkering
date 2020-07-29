266d9e45bab504032880ccfec9389f0248df27b2
Miguel Valarao <miguel.valarao@gmail.com>
2020-07-29 00:29:57 -0700

    10.1 Generic data types

---
+++ 10_generics_traits_and_lifetimes/10.1_generic_data_types/generic_structs/src/main.rs
@@ -0,0 +1,24 @@
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+fn main() {
+    let p = Point { x: 5, y: 10 };
+    let q = Point {x: 5 as f32, y: 10 as f32 };
+    let qDist = q.distance_from_origin();
+
+    println!("p.x = {}", p.x());
+}
